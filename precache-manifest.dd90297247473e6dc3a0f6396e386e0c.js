25327a33fc8b08c954f7d5ffdde7948dfc48d464
Piyush Agarwal <51760520+Piyush55dude@users.noreply.github.com>
2020-05-17 00:00:51 +0530

    Updates

---
+++ precache-manifest.dd90297247473e6dc3a0f6396e386e0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2d7f32f6bb2f7fde08e7e06232e17f",
+    "url": "/covid19_tracer/index.html"
+  },
+  {
+    "revision": "7d4ee0f5c7edace3770e",
+    "url": "/covid19_tracer/static/css/main.65a0042f.chunk.css"
+  },
+  {
+    "revision": "a344eb5e0280c3e0bff8",
+    "url": "/covid19_tracer/static/js/2.82feb3ce.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid19_tracer/static/js/2.82feb3ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d4ee0f5c7edace3770e",
+    "url": "/covid19_tracer/static/js/main.6fa025c5.chunk.js"
+  },
+  {
+    "revision": "b2964ee23301ee307a83",
+    "url": "/covid19_tracer/static/js/runtime-main.583c96bb.js"
+  },
+  {
+    "revision": "0eeb8424526dd39edbd28d9866a3a6e7",
+    "url": "/covid19_tracer/static/media/image.0eeb8424.png"
+  }
+]);
